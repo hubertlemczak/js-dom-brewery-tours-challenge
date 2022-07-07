864d83ba67fb7f1761b591f8e7271c16da683d78
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-07 13:24:31 +0100

    finished extension 1

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 const state = {
   breweries: [],
+  displayedBreweries: [],
+  filter: {
+    byType: {
+      applied: false,
+      value: '',
+    },
+    bySearch: {
+      applied: false,
+      value: '',
+    },
+    byState: {
+      applied: false,
+      value: '',
+    },
+  },
+};
+
+const setState = (newState) => {
+  console.log('newState', newState);
+  Object.keys(newState).forEach((key) => {
+    state[key] = newState[key];
+  });
+  console.log('state', state);
+  render();
+};
+
+const setDisplayedBreweries = () => {
+  if (state.filter.byType.applied) filterByType();
+  else {
+    const breweries = [...state.breweries];
+    console.log('brews', breweries);
+    setState({ displayedBreweries: breweries });
+  }
+  if (state.filter.byState.applied) filterByState();
+  if (state.filter.bySearch.applied) filterBySearch();
 };
 
 const getBreweries = () => {
   const breweryTypes = [
-    'https://api.openbrewerydb.org/breweries?by_type=micro&per_page=50',
-    'https://api.openbrewerydb.org/breweries?by_type=regional&per_page=50',
-    'https://api.openbrewerydb.org/breweries?by_type=brewpub&per_page=50',
+    'https://api.openbrewerydb.org/breweries?by_type=micro&per_page=10',
+    'https://api.openbrewerydb.org/breweries?by_type=regional&per_page=10',
+    'https://api.openbrewerydb.org/breweries?by_type=brewpub&per_page=10',
   ];
   breweryTypes.forEach((type) =>
     fetch(type)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         state.breweries.push(data);
         state.breweries = state.breweries.flat();
-        render();
+        update();
       })
   );
 };
@@ -24,27 +59,103 @@ const filterByTypeListener = () => {
   const filterForm = document.querySelector('#filter-by-type-form');
   filterForm.addEventListener('input', (e) => {
     const type = filterForm.filterByType.value;
-    filterByType(type);
+    const newState = { ...state.filter };
+
+    if (type === 'all') {
+      newState.byType.applied = false;
+      newState.byType.value = 'all';
+    } else {
+      newState.byType.applied = true;
+      newState.byType.value = type;
+    }
+
+    setState({ filter: newState });
+    setDisplayedBreweries();
   });
 };
 
-const filterByType = (type) => {
-  console.log(type);
-  if (type === 'all') init();
-  else {
-    fetch(`https://api.openbrewerydb.org/breweries?by_type=${type}&per_page=50`)
-      .then((res) => res.json())
-      .then((data) => {
-        state.breweries = data;
-        render();
-      });
-  }
+const filterByType = () => {
+  const breweryByType = state.breweries.filter(
+    (brewery) => brewery.brewery_type === state.filter.byType.value
+  );
+  setState({ displayedBreweries: breweryByType });
 };
 // TEST CODE
-const render = () => {
+
+const searchBarListener = () => {
+  const searchBar = document.querySelector('#search-breweries');
+  searchBar.addEventListener('input', (e) => {
+    const value = searchBar.value.toLowerCase();
+    const newState = { ...state.filter };
+
+    if (value.length > 0) {
+      newState.bySearch.applied = true;
+      newState.bySearch.value = value;
+    } else {
+      newState.bySearch.applied = false;
+      newState.bySearch.value = value;
+    }
+
+    setState({ filter: newState });
+    setDisplayedBreweries();
+  });
+};
+
+const filterBySearch = () => {
+  const breweries = [...state.displayedBreweries];
+  const filtered = breweries.filter((brewery) =>
+    brewery.name.toLowerCase().includes(state.filter.bySearch.value)
+  );
+  setState({ displayedBreweries: filtered });
+};
+
+const stateSearchFormListener = () => {
+  const stateSearchForm = document.querySelector('#select-state-form');
+  stateSearchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = stateSearchForm.selectState.value.toLowerCase();
+    const newState = { ...state.filter };
+
+    if (value.length > 0) {
+      newState.byState.applied = true;
+      newState.byState.value = value;
+    } else {
+      newState.byState.applied = false;
+      newState.byState.value = value;
+    }
+
+    setState({ filter: newState });
+    setDisplayedBreweries();
+  });
+};
+
+const filterByState = () => {
+  const breweries = [...state.displayedBreweries];
+  const filtered = breweries.filter((brewery) => {
+    if (brewery.state)
+      return brewery.state.toLowerCase().includes(state.filter.byState.value);
+  });
+  setState({ displayedBreweries: filtered });
+};
+
+// const renderSearchBar = () => {
+//   const breweriesARTICLE = document.querySelector('.breweries-search-bar');
+//   breweriesARTICLE.innerHTML = '';
+//   breweriesARTICLE.innerHTML = `
+//     <header class="search-bar">
+//       <form id="search-breweries-form" autocomplete="off">
+//         <label for="search-breweries"><h2>Search breweries:</h2></label>
+//         <input id="search-breweries" name="searchBreweries" type="text">
+//       </form>
+//     </header>
+//   `;
+// };
+
+const renderBreweries = () => {
   const breweriesUL = document.querySelector('#breweries-list');
   breweriesUL.innerHTML = '';
-  state.breweries.forEach((brewery) => {
+  const breweries = state.displayedBreweries;
+  breweries.forEach((brewery) => {
     breweriesUL.innerHTML += `
     <li>
       <h2>${brewery.name || 'N/A'}</h2>
@@ -52,7 +163,7 @@ const render = () => {
       <section class="address">
         <h3>Address:</h3>
         <p>${brewery.street || 'N/A'}</p>
-        <p><strong>${brewery.state || 'N/A'}, ${
+        <p><strong>${brewery.state || 'N/A'}: ${brewery.city || 'N/A'}, ${
       brewery.postal_code || 'N/A'
     }</strong></p>
       </section>
@@ -70,9 +181,21 @@ const render = () => {
   });
 };
 
-const init = () => {
-  getBreweries();
+const render = () => {
+  // renderSearchBar();
+  renderBreweries();
 };
 
+const update = () => {
+  setDisplayedBreweries();
+  render();
+};
+
+const init = () => {
+  stateSearchFormListener();
+  filterByTypeListener();
+  searchBarListener();
+};
+
+getBreweries();
 init();
-filterByTypeListener();
