4985f6ff1e913e3b0dfd747663743f54f4437b81
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-07 03:09:08 +0100

    added type filter

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const state = {
   breweries: [],
-  filterType: null,
-  filteredBreweries: [],
 };
 
-const breweryTypes = [
-  'https://api.openbrewerydb.org/breweries?by_type=micro&per_page=50',
-  'https://api.openbrewerydb.org/breweries?by_type=regional&per_page=50',
-  'https://api.openbrewerydb.org/breweries?by_type=brewpub&per_page=50',
-];
-breweryTypes.forEach((type) =>
-  fetch(type)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      state.breweries.push(data);
-      state.breweries = state.breweries.flat();
-      render();
-    })
-);
+const getBreweries = () => {
+  const breweryTypes = [
+    'https://api.openbrewerydb.org/breweries?by_type=micro&per_page=50',
+    'https://api.openbrewerydb.org/breweries?by_type=regional&per_page=50',
+    'https://api.openbrewerydb.org/breweries?by_type=brewpub&per_page=50',
+  ];
+  breweryTypes.forEach((type) =>
+    fetch(type)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.breweries.push(data);
+        state.breweries = state.breweries.flat();
+        render();
+      })
+  );
+};
+
+const filterByTypeListener = () => {
+  const filterForm = document.querySelector('#filter-by-type-form');
+  filterForm.addEventListener('input', (e) => {
+    const type = filterForm.filterByType.value;
+    filterByType(type);
+  });
+};
+
+const filterByType = (type) => {
+  console.log(type);
+  if (type === 'all') init();
+  else {
+    fetch(`https://api.openbrewerydb.org/breweries?by_type=${type}&per_page=50`)
+      .then((res) => res.json())
+      .then((data) => {
+        state.breweries = data;
+        render();
+      });
+  }
+};
 
 const render = () => {
   const breweriesUL = document.querySelector('#breweries-list');
@@ -48,3 +69,10 @@ const render = () => {
       `;
   });
 };
+
+const init = () => {
+  getBreweries();
+};
+
+init();
+filterByTypeListener();
